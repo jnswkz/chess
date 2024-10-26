f193c9d9c9dffd582120f9767769b4a28c3a3127
jnswkz <23520408@gm.uit.edu.vn>
2024-10-26 14:40:58 +0700

    init

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using chessboard;
+
+class Program {
+    static void Main(string[] args) {
+        Map mapinstance = new Map();
+        mapinstance.str();
+    }
+}
---
+++ src/map.cs
@@ -0,0 +1,8 @@
+namespace chessboard
+{
+    class Map {
+        public void str() {
+            System.Console.WriteLine("calling map");
+        }
+    }
+}
