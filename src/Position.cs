d77c2a0639ef9b825260d0373168c1ad3f1bf974
ROSEJC <123725716+ROSEJC@users.noreply.github.com>
2024-10-27 17:36:52 +0700

    Add files via upload

---
+++ src/Position.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Position : MonoBehaviour
+{
+    int x, y;
+    void Start()
+    {
+        x = 0;
+        y = 0;
+    }
+
+    public void setPos(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector2 getPos()
+    {
+        return new Vector2(x, y); 
+    }
+}
