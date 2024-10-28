1cbddf2d960fd3eb440111703a0304f3381effd8
cuogne <cuog.nc116@gmail.com>
2024-10-28 14:48:25 +0700

    commit test by em cuong

--- src/map.cs
+++ src/map.cs
@@ -3,6 +3,7 @@ namespace chessboard
     class Map {
         public void str() {
             System.Console.WriteLine("calling map");
+            System.Console.WriteLine("commit test tu em cuog");
         }
     }
 }
