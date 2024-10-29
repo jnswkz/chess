97b8852430ed6ea570bc986d84d14224fe957185
jnswkz <23520408@gm.uit.edu.vn>
2024-10-29 15:39:42 +0700

    update Position, upload pieces files

--- src/map.cs
+++ src/map.cs
@@ -5,7 +5,6 @@ namespace chessboard
     public class Map {
         public void str() {
             System.Console.WriteLine("calling map");
-            System.Console.WriteLine("commit test tu em cuog");
         }
     }
 }
